a72d8d347383ef13440c7b162545b85b89711066
DESKTOP-F3EK6GQ\calho <calhoun.carl@gmail.com>
2019-01-20 00:18:41 -0500

    Updated factions to follow STALKER based models.

--- schema/factions/sh_duty.lua
+++ schema/factions/sh_duty.lua
@@ -4,6 +4,7 @@
 
 FACTION.name = "Duty"
 FACTION.description = "Founded by former military expedition survivors and Stalkers concerned about the looming horrors of the Zone."
+FACTION.models = {"models/tnb/stalker/male_01_anorak.mdl","models/tnb/stalker/male_02_anorak.mdl","models/tnb/stalker/male_03_anorak.mdl","models/tnb/stalker/male_04_anorak.mdl","models/tnb/stalker/male_05_anorak.mdl","models/tnb/stalker/male_06_anorak.mdl","models/tnb/stalker/male_07_anorak.mdl","models/tnb/stalker/male_08_anorak.mdl","models/tnb/stalker/male_09_anorak.mdl","models/tnb/stalker/male_10_anorak.mdl","models/tnb/stalker/female_01_anorak.mdl","models/tnb/stalker/female_02_anorak.mdl","models/tnb/stalker/female_03_anorak.mdl","models/tnb/stalker/female_04_anorak.mdl","models/tnb/stalker/female_05_anorak.mdl","models/tnb/stalker/female_06_anorak.mdl","models/tnb/stalker/female_07_anorak.mdl","models/tnb/stalker/female_08_anorak.mdl"}
 FACTION.isDefault = false
 FACTION.color = Color(100, 60, 60)
 
--- schema/factions/sh_freedom.lua
+++ schema/factions/sh_freedom.lua
@@ -4,6 +4,7 @@
 
 FACTION.name = "Freedom"
 FACTION.description = "A clan of stalkers who fight for free access to the Zone, and their views are somewhat anarchistic, believing nobody can own the Zone and seeing it both as a scientific marvel and as a free life inside the Zone's borders."
+FACTION.models = {"models/tnb/stalker/male_01_anorak.mdl","models/tnb/stalker/male_02_anorak.mdl","models/tnb/stalker/male_03_anorak.mdl","models/tnb/stalker/male_04_anorak.mdl","models/tnb/stalker/male_05_anorak.mdl","models/tnb/stalker/male_06_anorak.mdl","models/tnb/stalker/male_07_anorak.mdl","models/tnb/stalker/male_08_anorak.mdl","models/tnb/stalker/male_09_anorak.mdl","models/tnb/stalker/male_10_anorak.mdl","models/tnb/stalker/female_01_anorak.mdl","models/tnb/stalker/female_02_anorak.mdl","models/tnb/stalker/female_03_anorak.mdl","models/tnb/stalker/female_04_anorak.mdl","models/tnb/stalker/female_05_anorak.mdl","models/tnb/stalker/female_06_anorak.mdl","models/tnb/stalker/female_07_anorak.mdl","models/tnb/stalker/female_08_anorak.mdl"}
 FACTION.isDefault = false
 FACTION.color = Color(100, 60, 60)
 
