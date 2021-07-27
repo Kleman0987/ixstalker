8c8cdedcd649065b0504f71695c8c69d003e8a77
Kleman0987 <87995849+Kleman0987@users.noreply.github.com>
2021-07-27 15:24:24 +0900

    fdf

--- README.md
+++
@@ -1,3 +0,0 @@
-
-# S.T.A.L.K.E.R. Schema
-A S.T.A.L.K.E.R. Roleplay schema for [Helix](https://github.com/nebulouscloud/helix).
--- schema/items/bags/sh_backpack.lua
+++
@@ -1,5 +0,0 @@
-ITEM.name = "Backpack"
-ITEM.description = "A massive backpack."
-ITEM.model = Model("models/stalker/item/handhelds/files1.mdl")
-ITEM.invWidth = 20
-ITEM.invHeight = 20
